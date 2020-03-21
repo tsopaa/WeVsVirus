e9251be6ebc04a064dbdc85a55ca0f4c11e314a3
Denzell92 <s0558978@htw-berlin.de>
2020-03-21 12:11:26 +0100

    Add checkBoxes

--- src/startpage-screen.js
+++ src/startpage-screen.js
@@ -1,5 +1,7 @@
 import { PolymerElement, html } from "@polymer/polymer/polymer-element.js";
 
+import "@polymer/paper-checkbox/paper-checkbox.js";
+
 class StartpageScreen extends PolymerElement {
   static get is() {
     return "startpage-screen";
@@ -13,21 +15,67 @@ class StartpageScreen extends PolymerElement {
         .random-btn {
           background-color: darkgreen;
           color: white;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
+          position: relative;
           width: 40%;
+          margin-left: 30%;
+        }
+
+        .text-container {
+          position: relative;
+          width: 80%;
+          font-size: 26px;
+          text-align: center;
+          color: white;
+          margin: 20% 10% 10% 10%;
+
+        }
+
+        .checkBox {
+          position: relative;
+          --paper-checkbox-unchecked-color: white;
+          /*--paper-checkbox-checked-color: black; blue*/
+          --paper-checkbox-label-color: white;
+          display: block;
+          left: 50%;
+          transform: translateX(-50%);
+
+        }
+
+        .checkbox-container {
+          position: relative;
+          width: 90%;
+          margin: 5% 0 0 5%;
+          font-size: 18px;
+
+        }
+
+        .inner-checkbox-container {
+          margin-left: 10%;
         }
 
         @media (max-width: 700px) {
           .random-btn {
             width: 80%;
+            margin-left: 10%;
           }
         }
       </style>
-
+      <div class="text-container">
+        Deine täglichen Quarantäne-Vorschläge
+      </div>
       <paper-button class="random-btn" on-click="_dispatchRandomBtnClickedEvent">Random</paper-button>
+
+      <div class="checkbox-container">
+        <div class="inner-checkbox-container">
+          <paper-checkbox class="checkBox">Unchecked</paper-checkbox>
+          <paper-checkbox class="checkBox">UncheckedUNCHECKED</paper-checkbox>
+          <paper-checkbox class="checkBox">Unchecked</paper-checkbox>
+          <paper-checkbox class="checkBox">Unchecked</paper-checkbox>
+          <paper-checkbox class="checkBox">Unchecked</paper-checkbox>
+          <paper-checkbox class="checkBox">Unchecked</paper-checkbox>
+          <paper-checkbox class="checkBox">Unchecked</paper-checkbox>
+        </div>
+      </div>
     `;
   }
 
